693cfc2a17153f87c92827de1f44e5285b5c905b
Nguyen Mau Hai Dang <nmhdang0107cr@gmail.com>
2022-05-25 21:14:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/vsocial/RegisterActivity.java
@@ -0,0 +1,120 @@
+package com.example.vsocial;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.style.UpdateAppearance;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    //views
+    EditText mEmailEt, mPasswordEt;
+    Button mRegisterBtn;
+
+    ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //Actionbar and its title
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Tạo Tài Khoản");
+        //enable back button
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        //init
+        mEmailEt = findViewById(R.id.emailEt);
+        mPasswordEt = findViewById(R.id.passwordEt);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+
+        //in the onCreate() method initialize the firebaseauth instance
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //progress
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Đang Tạo Tài Khoản Người Dùng...");
+
+        //handle click register btn click
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //input email, password
+                String email = mEmailEt.getText().toString().trim();
+                String password = mPasswordEt.getText().toString().trim();
+                //validate
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    //set error and focuss to email edittext
+                    mEmailEt.setError("Email đã đăng ký");
+                    mEmailEt.setFocusable(true);
+                }
+                else if (password.length()<6){
+                    //set error and focuss to email edittext
+                    mPasswordEt.setError("Mật khẩu phải dài ít nhất 6 ký tự");
+                    mPasswordEt.setFocusable(true);
+                }
+                else {
+                    registerUser(email, password); //register the user
+                }
+            }
+        });
+    }
+
+    private void registerUser(String email, String password) {
+        //email and password pattern is valid, show progress dialog and start registering user
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //sign in success, dismiss dialog and start register activity
+                            progressDialog.dismiss();
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Toast.makeText(RegisterActivity.this, "Đã đăng ký thành công...\n"+user.getEmail(), Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
+                            finish();
+                        } else {
+                            //if sign in fails, display a messge to the user.
+                            progressDialog.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Đã xảy ra lỗi khi xác thực tài khoản", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //error, dismiss progress dialog and get and show the error message
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
